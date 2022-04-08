cb44c15519f02402687185aad4e03d89db867a21
Marcus Paulo <marcuspaulorralves@outlook.com>
2022-04-08 10:18:49 -0300

    Showing captured pieces Little BUGGED

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,12 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
+	
+	
+	
 
 	public ChessMatch() {
 		board = new Board(8, 8);
@@ -67,6 +76,11 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) { 
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	
@@ -99,9 +113,11 @@ public class ChessMatch {
 		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 
+	//Coloca as peças no tabuleiro
 	private void placeNewPiece(char clomun, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(clomun, row).toPosition());
-
+		//coloca as peças tomadas na lista
+		piecesOnTheBoard.add(piece);
 	}
 
 	private void initialSetup() {
